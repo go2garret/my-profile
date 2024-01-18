d7ac785a652045d98345b8c156b9020fa43883d5
G K <gk@setup.com>
2024-01-17 20:52:50 -0500

    active page bugfix

--- my-profile/src/pages/Layout.vue
+++ my-profile/src/pages/Layout.vue
@@ -9,15 +9,15 @@
 
             <div class="flex bg-gray-800 text-gray-300 rounded-3xl mb-2 p-3"
             style="box-shadow: 0 4px 6px -1px #0000001a,0 2px 4px -1px #0000000f,0 10px 15px -3px #0000001a,0 20px 25px -5px #0000001a,0 25px 50px -12px #00000040">
-                <b>...{{ active }}...</b>
+                
                 <div v-for="(val, i) in pages" 
                 class="flex items-center justify-center"
                 :key="'btn'+val.name">
-                    {{ val.name }}
+                
                     <RadioButton
                     @click="setActive(val.name)"
                     @mousemove="onMouseover(val.title, $event)"
-                    :active="active == val.name">
+                    :active="(active == '/' || !active ? 'welcome' : active) == val.name">
                     </RadioButton>
 
                 </div>                
@@ -59,7 +59,6 @@
         },
 
         mounted() {
-            // this.handleRoute();
         },
 
         methods: {
@@ -71,8 +70,7 @@
             },
             
             setActive(page) {
-                console.log('setactive', page);   
-                this.active = page;                            
+                console.log('setactive', page);                         
                 this.$router.push(`${page}`);
                 this.$emit('setActive', page);
             },
